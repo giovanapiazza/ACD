546fb3c9047fdf73e4d8f527513ff53f422cccaa
Giovana Piazza <135471104+giovanapiazza@users.noreply.github.com>
2025-09-17 16:03:53 -0300

    Create Cliente.java

---
+++ Supermecardo/Cliente.java
@@ -0,0 +1,11 @@
+package supermercado;
+
+public class Cliente {
+    String nome;
+    String cpf;
+
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+}
